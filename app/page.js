8f2722648a7c852f6b5887d260cfa7661f78f3c3
FastRives <57791635+FastRives@users.noreply.github.com>
2024-08-16 18:59:58 +0200

    Test page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,25 @@
+'use client'
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import { isMobile } from 'react-device-detect';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [contenueToShow, setContenueToShow] = useState(<></>);
+  
+  useEffect(() => {
+    if(isMobile){
+      setContenueToShow(<p>Vous êtes actuellement sur MOBILE.</p>);
+    } else {
+      setContenueToShow(<p>Retourne sur ton téléphone !</p>);
+    }
+  }, [isMobile])
+  
+  
   return (
     <main className={styles.main}>
-      {isMobile 
-        ? <p>Nous sommes sur MOBILE.</p>
-        : <p>Retourne sur ton téléphone !</p>
-      }
+      {contenueToShow}
     </main>
   )
 }
