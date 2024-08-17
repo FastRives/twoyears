cca22011e1e4bb77d81794d00d6876e20aea0d60
FastRives <57791635+FastRives@users.noreply.github.com>
2024-08-17 13:37:26 +0200

    patch Apos

--- components/bienvenue/bienvenue.jsx
+++ components/bienvenue/bienvenue.jsx
@@ -7,7 +7,7 @@ const Bienvenue = () => {
             <h1>Bienvenue sur les traces de ta chasse au trésor !</h1>
         </div>
         <div className="corps">
-            <p>Ici tu vas devoir découvrir et débloquer l'accés à chaque étape, en te remémorant des moments passés ensemble, il n'y aura pas d&pos;indices, le seul indice, c'est ta mémoire...</p>
+            <p>Ici tu vas devoir découvrir et débloquer laccés à chaque étape, en te remémorant des moments passés ensemble, il n&apos;y aura pas d&pos;indices, le seul indice, c&apos;est ta mémoire...</p>
             <button>Commencer la chasse au trésor !</button>
         </div>
     </div>
